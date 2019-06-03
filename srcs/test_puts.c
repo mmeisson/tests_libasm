347d004689790b84750e127ba874e15cd786eb35
mmeisson <mmeisson@student.42.fr>
2019-06-03 14:51:27 +0200

    Comment test_puts

--- srcs/test_puts.c
+++ srcs/test_puts.c
@@ -10,8 +10,36 @@ int		ft_puts(const char *str);
 
 extern int g_verbose;
 
+
 char	*get_puts(char *test, unsigned long test_size, int (*p_puts)(const char *str))
 {
+	/*
+	 * How this function works ? ( man 2 dup ; man 2 pipe ; man 2 fork ; man 2 read )
+	 *
+	 * General explanations :
+	 * To retrieve puts's output in a string, we create a second process.
+	 * The second process will write in a file descriptor readable by the
+	 * first process. So the first process just have to read the output
+	 * with the associated syscall in order to have it in a string
+	 *
+	 *
+	 * More in-depth :
+	 *  'pipe' is a syscall that creates a pair of file descriptor, you can
+	 *  read things on the first fd that have been written previously on
+	 *  the second file descriptor.
+	 *  'fork' creates a new process.
+	 *  Using both 'pipe' and 'fork' allows to have to process that can
+	 *  communicate ( through file descriptors ).
+	 *  'dup2' allows to "replace" a file descriptor. Given the file
+	 *  descriptor 1, if you dup2 3 on 1, it means that writting on 1
+	 *  will more or less the same as writting on 3. So dupplicating
+	 *  the second file descriptor given by 'pipe' on 1 means that using
+	 *  puts, which is supposed to write on 1, will output in the pipe.
+	 *
+	 *  The first process, just have to read until the second process
+	 *  writes.
+	 *  Repeat the operation with ft_puts and puts, compare outputs
+	 */
 	int		pi[2];
 	char	buffer[test_size + 10];
 	char	*str = NULL;
@@ -85,9 +113,9 @@ int		test_puts(void)
 		{"a", 1},
 		{"ab", 2},
 		{"abc", 3},
-		{"Une chaine de caractere nettement plus longue", 46},
+		{"Une chaine de caractere nettement plus longue", 45},
 #ifdef __APPLE__
-		{NULL, 0},
+		{NULL, 0}, // man puts says that it has to be handled on macos. Not important imo
 #endif
 	};
 	int			test_status;
